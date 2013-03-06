1762b5848d9c37ff8483898fb88b32bd2f35ee00
Kai Borowiak <kai.borowiak@secunet.com>
2013-03-06 15:37:29 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name="colored-logging",
+	version="1.0",
+	description="Enables colored log messages for Python's default logging module",
+	author="Kai Borowiak",
+	modules=['colored_logging'],
+	install_requires=[
+		'colorama>=0.2.5'
+	],
+)
