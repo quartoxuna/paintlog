9f0e48703356855d2933327f90c0be9ff9fe56f3
Kai Borowiak <kai.borowiak@secunet.com>
2013-03-13 11:20:41 +0100

    Changed name to paintlog, added R2D2 as dependency link

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@
 from setuptools import setup
 
 setup(
-	name="colored-logging",
+	name="paintlog",
 	version="1.0",
 	description="Enables colored log messages for Python's default logging module",
 	author="Kai Borowiak",
-	packages=['colored_logging'],
+	packages=['paintlog'],
 	install_requires=[
 		'colorama>=0.2.5'
+	],
+	dependency_links=[
+		'http://192.168.2.20/basket',
 	],
 )
