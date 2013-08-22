36068c1fd97a18cbd0944a09605e00b2e7fde260
Kai Borowiak <kai.borowiak@secunet.com>
2013-08-22 11:28:58 +0200

    Refactored setup script

--- setup.py
+++ setup.py
@@ -7,5 +7,20 @@ setup(
 	version="1.1",
 	description="Enables colored log messages for Python's default logging module",
 	author="Kai Borowiak",
+	author_email="kai.borowiak@secunet.de",
 	packages=['paintlog'],
+	long_description="""\
+	Provides custom Formatter using the 'colorama' module
+	to generate colored log messages in Python.
+	""",
+	classifiers=[
+		"License :: OSI Approved :: GNU General Public License (GPL)",
+		"Programming Language :: Python",
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+	],
+	keywords='python logging colorama',
+	license='GPL',
+	install_requires=['colorama'],
+	dependency_links=['http://sinaqs.secuent.de/pybasket/']
 )
