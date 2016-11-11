45ca13b3ac94493ada43bece9c90911425d4c490
Kai Borowiak <kai.borowiak@secunet.com>
2016-11-11 11:03:38 +0100

    Refactored

--- paintlog.py
+++ paintlog.py
@@ -7,64 +7,64 @@
 @summary: Colored string formatting for Python's default logging module
 """
 
+__all__ = ["ColoredFormatter","Foreground","Background","Style"]
+
 import colorama
 import logging
 
-from colorama import Fore
-from colorama import Back
-
+import colorama
 colorama.init()
 
-class Formatter(logging.Formatter):
-	"""Colored formatter
-	@cvar coloring: Default color rules for the default log levels
-	@type coloring: dict
-	"""
-	coloring = {
-	        'DEBUG': Fore.WHITE,
-	        'INFO': Fore.WHITE,
-	        'WARNING': Fore.WHITE,
-	        'ERROR': Fore.WHITE,
-	        'CRITICAL': Fore.WHITE
-	}
+from colorama import Fore as Foreground
+from colorama import Back as Background
+from colorama import Style
 
-	def __init__(self,fmt=None,datefmt=None,bright=True):
-		logging.Formatter.__init__(self,fmt,datefmt)
-		self.__rules = Formatter.coloring
-		self.__bright = bright
+class ColoredFormatter(logging.Formatter):
+    """Colored formatter
+    @cvar coloring: Default color rules for the default log levels
+    @type coloring: dict
+    """
 
-	def setColor(self,level=None,color=None,**kwargs):
-		"""Changes color definitions for log levels.
-		@param level: The Level to change
-		@type level: int
-		@param color: The color for the level
-		@type color: int
-		@param kwargs: Multi rule setting
-		@type kwargs: **kwargs
-		"""
-		if level and color:
-			self.__rules[level] = color
-			if self.__bright:
-				self.__rules[level] += colorama.Style.BRIGHT
-		elif len(kwargs)>0:
-			for level,color in kwargs.items():
-				self.setColor(level,color)
-		
-	def format(self, record):
-		"""Extends default formatting.
-		@param record: The log record to format
-		@type record: LogRecord
-		@returns: Formatted and colored string
-		@rtype: str
-		"""
-		msg = logging.Formatter.format(self,record)
-		levelname = logging.getLevelName(record.levelno)
-		
-		# Insert colors
-		msg = msg.replace("<color>",self.__rules[levelname]).replace("</color>",colorama.Style.RESET_ALL)
-		
-		# Insert styles
-		msg = msg.replace("<b>",colorama.Style.DIM).replace("</b>",colorama.Style.RESET_ALL)
-		
-		# Return new formatted message
-		return msg
+    DEFAULT_RULES = {\
+                        logging.DEBUG: Foreground.GREEN,\
+                        logging.INFO: Foreground.CYAN,\
+                        logging.WARNING: Foreground.MAGENTA,\
+                        logging.ERROR: Foreground.RED,\
+                        logging.CRITICAL: Foreground.WHITE + Background.RED\
+                    }
+
+    def __init__(self, *args, **kwargs):
+        logging.Formatter.__init__(self, *args, **kwargs)
+        # Set default rules
+        self._rules = ColoredFormatter.DEFAULT_RULES
+
+    def setRule(self,level=None,color=None,**kwargs):
+        """Changes color definitions for log levels.
+        @param level: The Level to change
+        @type level: int
+        @param color: The color for the level
+        @type color: int
+        @param kwargs: Multi rule setting
+        @type kwargs: **kwargs
+        """
+        if level and color:
+            self._rules[level] = color
+        elif len(kwargs)>0:
+            for level,color in kwargs.items():
+                self.setColor(level,color)
+
+    def format(self, record):
+        """Extends default formatting.
+        @param record: The log record to format
+        @type record: LogRecord
+        @returns: Formatted and colored string
+        @rtype: str
+        """
+        levelno = record.levelno
+        record.levelname = self._rules[levelno] + record.levelname
+        # Reset coloring
+        record.levelname += Style.RESET_ALL
+        return logging.Formatter.format(self,record)
+
+# Set ourself as new default formatter
+logging._defaultFormatter = ColoredFormatter()
--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@
 from setuptools import setup
 
 setup(
-	name="paintlog",
-	version="1.2",
-	description="Enables colored log messages for Python's default logging module",
-	author="Kai Borowiak",
-	author_email="kai.borowiak@secunet.de",
-	py_modules=['paintlog'],
-	keywords='python logging colorama',
-	license='GPL',
-	dependency_links=['http://sinaqs.secunet.de/pypi/'],
-	install_requires=[
-		'colorama',
-	],
+    name="paintlog",
+    version="2.0",
+    description="Enables colored log messages for Python's default logging module",
+    author="Kai Borowiak",
+    author_email="kai.borowiak@secunet.de",
+    py_modules=['paintlog'],
+    keywords='python logging colorama',
+    license='GPL',
+    dependency_links=['http://sinaqs.secunet.de/pypi/'],
+    install_requires=[
+        'colorama',
+    ],
 )
