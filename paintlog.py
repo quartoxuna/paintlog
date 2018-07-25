d51e6c64465a16af9c397194b748bfc655fbf419
Kai Borowiak <kai.borowiak@secunet.com>
2018-07-25 10:44:29 +0200

    Add doc

--- paintlog.py
+++ paintlog.py
@@ -1,10 +1,73 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
-@author: Kai Borowiak
-@requires: logging>=2.7
-@requires: colorama>=0.2.5
-@summary: Colored string formatting for Python's default logging module
+PaintLog
+========
+
+Colored Formatter for Python.
+
+.. rubric:: Example:
+
+.. code-block:: python
+
+   import logging
+   import paintlog
+
+   logger = logging.getLogger()
+   logger.setLevel(logging.DEBUG)
+
+   ch = logging.StreamHandler()
+   ch.setLevel(logging.DEBUG)#
+
+   # Create Colored Formatter like normal formatter
+   formatter = paintlog.ColoredFormatter('%(asctime)s [%(levelname)s] %(message)s')
+
+   ch.setFormatter(fmt)
+   logger.addHandler(ch)
+
+   # Log various messages
+   logger.debug("Debug")
+   logger.warning("Warning")
+   logger.info("Info")
+   logger.error("Error")
+   logger.critical("Critical")
+
+.. rubric:: Output:
+
+.. raw:: html
+
+   <div style='background-color:black;'>
+       <div style='color:green;font-family:Courier New;'>Debug</div>
+       <div style='color:magenta;font-family:Courier New;'>Warning</div>
+       <div style='color:cyan;font-famiy:Courier New;'>Info</div>
+       <div style='color:red;font-family:Courier New;'>Error</div>
+       <div style='background-color:red;color:white;font-family:Courier New;'>Critical</div>
+   </div>
+
+.. rubric:: Change Color Definitions:
+
+You can change the Coloring on the fly via the **__setitem__** method.
+
+.. code-block:: python
+
+   fmt['DEBUG']    = paintlog.Foreground.CYAN
+   fmt['WARNING']  = paintlog.Background.MAGENTA + paintlog.Foreground.WHITE
+   fmt['INFO']     = paintlog.Foreground.WHITE
+   fmt['ERROR']    = paintlog.Background.RED + paintlog.Foreground.WHITE
+   fmt['CRITICAL'] = paintlog.Foreground.RED
+
+..  rubric:: Output:
+
+.. raw:: html
+
+   <div style='background-color:black;'>
+       <div style='color:cyan;font-family:Courier New;'>Debug</div>
+       <div style='background-color:magenta;color:white;font-family:Courier New;'>Warning</div>
+       <div style='color:white;font-famiy:Courier New;'>Info</div>
+       <div style='background-color:red;color:white;font-family:Courier New;'>Error</div>
+       <div style='color:red;font-family:Courier New;'>Critical</div>
+   </div>
 """
 
 __all__ = ["ColoredFormatter","Foreground","Background","Style"]
@@ -25,11 +88,6 @@ colorama_init()
 ALIGNMENT_REGEX = re.compile("%\(levelname\)-?(?P<alignment>\d*)s")
 
 class ColoredFormatter(logging.Formatter):
-    """Colored formatter
-    @cvar coloring: Default color rules for the default log levels
-    @type coloring: dict
-    """
-
 
     def __init__(self, *args, **kwargs):
         logging.Formatter.__init__(self, *args, **kwargs)
