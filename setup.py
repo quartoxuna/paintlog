423bef9b037f7418827bb68f2c1ea2a0f3cf9dba
Kai Borowiak <kai.borowiak@secunet.com>
2019-02-22 09:42:59 +0100

    Update setup script

--- setup.py
+++ setup.py
@@ -7,21 +7,32 @@ from setuptools import setup
 exec(compile(open('./paintlog.py').read(), './paintlog.py', 'exec'))
 
 setup(
+    # Summary
     name="paintlog",
     version=__version__,
     description="Enables colored log messages for Python's default logging module",
     author="Kai Borowiak",
     author_email="kai.borowiak@secunet.de",
-    py_modules=['paintlog'],
     keywords='python logging colorama',
-    license='GPL',
+    license='GPLv3',
+    classifiers = [
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+
+    # Contents
+    py_modules=['paintlog'],
+    test_suite='test',
+
+    # Dependencies
     dependency_links=['http://sinaqs.secunet.de/pypi/'],
+
     install_requires=[
         'colorama',
     ],
+
     tests_require=[
         'mock',
-    ],
-    test_suite='test',
-    zip_safe=False,
+    ]
 )
